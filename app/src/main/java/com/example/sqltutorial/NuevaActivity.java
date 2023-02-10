1ed2ff929b3091830fc84acd9a9d36e4faaee407
abascunana <abascunanasanchez@cifpfbmoll.eu>
2023-02-10 16:50:26 +0100

    Spinner comentario

--- app/src/main/java/com/example/sqltutorial/NuevaActivity.java
+++ app/src/main/java/com/example/sqltutorial/NuevaActivity.java
@@ -4,15 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.sqltutorial.db.DBcomments;
 
+import java.util.List;
+
 public class NuevaActivity extends AppCompatActivity {
     EditText name,comment,namerq;
+    Spinner mSpinner;
     Button submmit;
     TextView showcom;
 
@@ -31,12 +37,26 @@ public class NuevaActivity extends AppCompatActivity {
         comment = (EditText) findViewById(R.id.editComm);
         submmit = (Button) findViewById(R.id.btnSubmit);
         request = (Button) findViewById(R.id.btnrequest);
+        mSpinner = (Spinner) findViewById(R.id.cmntspp);
+        loadSpinnerData();
+        mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
         request.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 DBcomments dBcomments= new DBcomments(NuevaActivity.this);
                 String comment = dBcomments.SelectComment(namerq.getText().toString());
                 showcom.setText(comment);
+
             }
         });
         submmit.setOnClickListener(new View.OnClickListener() {
@@ -47,8 +67,22 @@ public class NuevaActivity extends AppCompatActivity {
                 if (id > 0){
                     Toast.makeText(NuevaActivity.this,"insertado",Toast.LENGTH_LONG);
                     limpiar();
+                    loadSpinnerData();
                 }
             }
         });
+    }
+    private void loadSpinnerData() {
+        DBcomments db = new DBcomments(getApplicationContext());
+        List<String> labels = db.getAllLabels();
+
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item, labels);
+
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        // attaching data adapter to spinner
+        mSpinner.setAdapter(dataAdapter);
     }
 }
--- app/src/main/java/com/example/sqltutorial/db/DbHelper.java
+++ app/src/main/java/com/example/sqltutorial/db/DbHelper.java
@@ -10,6 +10,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.Nullable;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class DbHelper  extends SQLiteOpenHelper {
     public static String DATABASENAME = "androidadvancesqlite";
@@ -22,6 +23,35 @@ public class DbHelper  extends SQLiteOpenHelper {
     public DbHelper(@Nullable Context context) {
         super(context, DATABASENAME, null, 1);
         c = context;
+    }
+    public List<String> getAllLabels(){
+        String selectQuery = "SELECT  nombre FROM " + COMMENTABLE;
+        List<String> list = new ArrayList<String>();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(selectQuery, null);//selectQuery,selectedArguments
+
+        try {
+
+
+            // Select All Query
+
+
+            // looping through all rows and adding to list
+            if (cursor.moveToFirst()) {
+                do {
+                    list.add(cursor.getString(0));//adding 2nd column data
+                } while (cursor.moveToNext());
+            }
+        }
+        catch (Exception e){
+
+        }
+
+        // closing connection
+        cursor.close();
+        db.close();
+        // returning lables
+        return list;
     }
 
     @Override
